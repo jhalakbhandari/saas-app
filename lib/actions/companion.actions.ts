1c0cd7ffdc5389c70e42e94f06c0bb68b7384689
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2025-11-30 15:46:51 +0530

    added Companion Library

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -14,3 +14,38 @@ export const createCompanion = async (formData: CreateCompanion) => {
     throw new Error(error?.message || "Failed to Create a companion");
   return data[0];
 };
+
+export const getAllCompanions = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllCompanions) => {
+  const supabase = createSupabaseClient();
+
+  let query = supabase.from("Companions").select("*");
+
+  // Both subject and topic
+  if (subject && topic) {
+    query = query
+      .ilike("subject", `%${subject}%`)
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  }
+  // Only subject
+  else if (subject) {
+    query = query.ilike("subject", `%${subject}%`);
+  }
+  // Only topic
+  else if (topic) {
+    query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  }
+
+  // Pagination
+  query = query.range((page - 1) * limit, page * limit - 1);
+
+  const { data: companions, error } = await query;
+
+  if (error) throw new Error(error.message);
+
+  return companions;
+};
