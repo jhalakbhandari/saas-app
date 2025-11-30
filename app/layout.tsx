f5c5106bb357d77a2af33f97191b090296d54097
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2025-11-30 11:54:06 +0530

    added clerk sign in

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+// import { type Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -20,8 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <Navbar/>{children}</body>
+      <ClerkProvider appearance={{ variables: { colorPrimary: "f35933" } }}>
+        <body className={`${bricolage.variable} antialiased`}>
+          <Navbar />
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
